b6bed1cab69aacaf55b4c37ef362d09ae1e5cc59
Steven Wong <syw14@sfu.ca>
2023-03-08 15:58:18 +0800

    complete file i/o and input redirection

--- assignments/PA2/hello-whoever.go
+++ assignments/PA2/hello-whoever.go
@@ -1,11 +1,14 @@
 package main
 
 import "fmt"
+import "os"
 
 func main() {
-  fmt.Printf("Who's there?\n")
+  fmt.Printf("Who's there? ")
   text := ""
   fmt.Scanf("%s", &text)
-  fmt.Printf("Hello, %s", text)
+  fmt.Printf("Hello, %s\n", text)
+  fmt.Println("Hello,", text)
+  fmt.Fprintf(os.Stdout, "Hello, %s\n", text)
 }
 
