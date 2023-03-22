feaa3d36ad6b2708f2997ee58bbf3eaa6a3afe8a
Steven Wong <syw14@sfu.ca>
2023-03-22 16:48:23 +0800

    draft ver complete, check against test output

--- assignments/PA3/PA3.go
+++ assignments/PA3/PA3.go
@@ -1,5 +1,4 @@
 // Objectives
-// Prompt the user for upload file name
 // Sends first the file size (# in size line)
 // Send the file contents
 // Receive a message back from server
@@ -9,10 +8,10 @@
 package main
 
 import (
-	// "bufio"
+	"bufio"
+	"fmt"
 	"net"
-	// "os"
-	// "fmt"
+	"os"
 )
 
 func check(e error) {
@@ -25,6 +24,42 @@ func main() {
 	// Verify that this is the command to connect to pollys server
 	conn, errc := net.Dial("tcp", "127.0.0.1:12000")
 	check(errc)
-	defer conn.Close()
 
+	// Prompt the user for upload file name
+	uploadFileName := ""
+	fmt.Print("Please enter the upload file name: ")
+	fmt.Scanf("%s", &uploadFileName)
+
+	// Open the file
+	file, errc := os.Open(uploadFileName)
+	check(errc)
+	defer file.Close()
+	// Calculate its file size
+	stat, errc := file.Stat()
+	check(errc)
+	sizeBytes := stat.Size()
+
+	// Create a buffered writer to send the file size
+	writer := bufio.NewWriter(conn)
+	// Send the file size
+	_, errw := writer.WriteString(fmt.Sprintf("%d", sizeBytes))
+	check(errw)
+	writer.Flush()
+
+	// Read the file contents and send it to the server at once
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		_, errw := writer.WriteString(scanner.Text())
+		check(errw)
+	}
+	writer.Flush()
+
+	// Read the server response
+	scanner = bufio.NewScanner(conn)
+	if scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+
+	// Close the connection and terminate the program
+	conn.Close()
 }
