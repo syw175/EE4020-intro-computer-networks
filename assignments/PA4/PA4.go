2f3feb3fb31e5de87def51ec4c2eb354776d1b25
Steven Wong <syw14@sfu.ca>
2023-04-02 20:51:48 +0800

    need testing on remote

--- assignments/PA4/PA4.go
+++ assignments/PA4/PA4.go
@@ -9,8 +9,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
+	"os"
 )
 
 // Function to check for errors, if not null, panic
@@ -21,11 +23,44 @@ func check(e error) {
 }
 
 func main() {
-	// Launch the server on local ip at port 12001
+	// Launch the server on local IP at port 12001
 	fmt.Println("Launching server... at port 12001")
 	ln, _ := net.Listen("tcp", ":12001")
 	conn, _ := ln.Accept()
-	ln.Close()
-	conn.Close()
+	defer ln.Close()
+	defer conn.Close()
+
+	// Read from the socket first the file size, demarcated by a newline
+	reader := bufio.NewReader(conn)
+	initialFileSize, err := reader.ReadString('\n')
+	check(err)
+
+	// Create a new file to write to
+	file, err := os.Create("whatever.txt")
+	check(err)
+	defer file.Close()
+
+	// While there are still lines to read, read one line at a
+	// time and prepend the line count to each line
+	lineCount := 0
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			break
+		}
+		lineCount++
+		newLine := fmt.Sprintf("%d %s", lineCount, line)
+		file.WriteString(newLine)
+	}
+
+	// Check the size of the new file
+	fileInfo, err := file.Stat()
+	check(err)
+	newFileSize := fileInfo.Size()
+
+	// Send a message to client the original and new file sizes
+	writer := bufio.NewWriter(conn)
+	writer.WriteString(fmt.Sprintf("Initial file size: %s", initialFileSize))
+	writer.WriteString(fmt.Sprintf("New file size: %d", newFileSize))
 
 }
