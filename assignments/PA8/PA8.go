06050c55d7471ea8a918031bf6d74fe7a0ed6277
Steven Wong <syw14@sfu.ca>
2023-06-04 16:42:08 +0800

    complete PA8

--- assignments/PA8/PA8.go
+++ assignments/PA8/PA8.go
@@ -1,40 +1,66 @@
 package main
 
 import (
-  "fmt"
-  "io/ioutils"
-  "net/http"
-  "net/url"
-  "strings"
-  "os"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
 )
 
-// Func to return the contents of a file in the current directory
-func listFile() []string {
-  var filename []string
-  // Return the files in the current directory
-  entries, err := ioutil.ReadDir("./")
-  if (err != nil) {
-	os.panic()
-  }
 
-  fmt.Println(entries)
-  for _, ent := range entries {
-	filename = append(filename, ent.Name())
-  }
-  return filename
+func check (e error){
+	if e != nil {
+		panic(e)
+	}
 }
 
-// Function to handle server requests
-func customHandler() {
+// return the content in the file directory
+func listFileDir() []string{
+	var filename []string
+	// return two values: the files in the directory, error
+	entries, err := ioutil.ReadDir("./")
+	check(err)
+
+	for _, ent := range entries {
+		filename = append(filename, ent.Name())
+	}
+	return filename
 }
 
-// Main function
-func main() {
-  fmt.Println("Launching server... ")
-  // Open the server for requests at port 12001
+// custom not found
+func notFound(w http.ResponseWriter, r *http.Request){
+	http.Error(w, "File not found.", http.StatusNotFound)
+}
 
+func customHandler(w http.ResponseWriter, r *http.Request){
+	files := listFileDir()
+	filepath := strings.TrimPrefix(r.URL.Path, "/")
+	found := false
+	for _, filename := range files {
+		if filepath == filename {
+			h := http.FileServer(http.Dir("."))
+			r2 := new(http.Request)
+			*r2 = *r
+			r2.URL = new(url.URL)
+			*r2.URL = *r.URL
+			r2.URL.Path = filepath
+            // main code looking for specified file in the filesystem
+			h.ServeHTTP(w, r2)
+			found = true
+			break
+		}
+	}
+
+	if !found {
+		notFound(w, r)
+	}
 
 }
 
-
+func main(){
+	fmt.Println("Launching server...")
+	hd := http.HandlerFunc(customHandler)
+	http.Handle("/", hd)
+	http.ListenAndServe(":12001", nil)
+}
